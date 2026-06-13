74cd1265f0f57e770524a4ca1a6be975d04a8d46
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-06-13 13:51:01 -0300

    melhorando o titulo

--- sistema.php
+++ sistema.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "******************\n";
-echo "Titular: Gabriel Almeida\n";
-echo "Saldo atual: R$ 1000\n";
-echo "******************\n";
+print_r("******************\n");
+print_r("Titular: Gabriel Almeida\n");
+print_r("Saldo atual: R$: 1000\n");
+print_r("******************\n");
